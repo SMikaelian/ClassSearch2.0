4707d61a8d81640e834711a63c8777ad943bae20
zamorapiero <47881531+zamorapiero@users.noreply.github.com>
2019-10-03 10:52:07 -0700

    Created Landing Page
    Hard coded the majors and created the drop down options with a form ready to be updated when we figure out hoe to query the API properly.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Class Search 2.0" />
+    <LandingPage msg="Class Search 2.0" />
     <p>Howdy</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import LandingPage from "@/components/LandingPage.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    LandingPage
   }
 };
 </script>
